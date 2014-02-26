97cc446a3c69752f01ea4aee02090d9a4e795d70
Julia Menchavez <jcmenchavez@gmail.com>
2014-02-26 20:31:41 +0800

    rows and columns inputs removed

--- Tester.java
+++ Tester.java
@@ -3,12 +3,10 @@ package denguesimulation;
 
 import javax.swing.*;
 
-public class Tester {
+public class Tester{
   public static void main(String[] args) {
     JTextField populationField = new JTextField(5);
     JTextField endTimeField = new JTextField(5);
-    JTextField rowsField = new JTextField(5);
-    JTextField columnsField = new JTextField(5);
     JTextField initInfectedPopulationField = new JTextField(5);
     JTextField infectionProbabilityField = new JTextField(5);
     JTextField resistanceRateField = new JTextField(5);
@@ -24,14 +22,6 @@ public class Tester {
     two.add(Box.createHorizontalStrut(90));
     two.add(new JLabel("End Time: "));
     two.add(endTimeField);
-    Box three = Box.createHorizontalBox();
-    three.add(Box.createHorizontalStrut(110));
-    three.add(new JLabel("Rows: "));
-    three.add(rowsField);
-    Box four = Box.createHorizontalBox();
-    four.add(Box.createHorizontalStrut(92));
-    four.add(new JLabel("Columns: "));
-    four.add(columnsField);
     Box five = Box.createHorizontalBox();
     five.add(new JLabel("Initial Infected Population: "));
     five.add(initInfectedPopulationField);
@@ -50,8 +40,6 @@ public class Tester {
       
     box.add(one);
     box.add(two);
-    box.add(three);
-    box.add(four);
     box.add(five);
     box.add(six);
     box.add(seven);
@@ -64,15 +52,12 @@ public class Tester {
     if (result == JOptionPane.OK_OPTION) {
       int population = Integer.parseInt(populationField.getText());
       int endTime = Integer.parseInt(endTimeField.getText());
-      int rows = Integer.parseInt(rowsField.getText());
-      int columns = Integer.parseInt(columnsField.getText());
       int initInfectedPopulation = Integer.parseInt(initInfectedPopulationField.getText());
       double infectionProbability = Double.parseDouble(infectionProbabilityField.getText());
       int resistanceRate = Integer.parseInt(resistanceRateField.getText());
       double reservoirProbability = Double.parseDouble(reservoirProbabilityField.getText());
       
-      DengueSimulation simulation = new DengueSimulation(population, endTime,
-        rows, columns, initInfectedPopulation, infectionProbability, resistanceRate, reservoirProbability);
+      DengueSimulation simulation = new DengueSimulation(population, endTime, initInfectedPopulation, infectionProbability, resistanceRate, reservoirProbability);
       simulation.simulate();
     } else;
   }
